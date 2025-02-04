70879045bb237fbedd525a6174cfda78e4b22ea8
Eran <eran.hasarali.2001@gmail.com>
2025-02-04 14:44:56 +0530

    change content

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import { motion } from "framer-motion";
+
 const App = () => {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="main">
+      <motion.div
+        className="animate-div"
+        animate={{ x: 1 }}
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+      >
+        <motion.h1 whileTap={{ color: "red" }}>Hello</motion.h1>
+      </motion.div>
+      <motion.div
+        className="animate-div"
+        animate={{ x: 1 }}
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+      >
+        <motion.h1 whileTap={{ color: "red" }}>It&apos;s Me</motion.h1>
+      </motion.div>
+      <motion.div
+        className="animate-div"
+        animate={{ x: 1 }}
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+      >
+        <motion.h1 whileTap={{ color: "red" }}>Eran</motion.h1>
+      </motion.div>
     </div>
   );
 };
